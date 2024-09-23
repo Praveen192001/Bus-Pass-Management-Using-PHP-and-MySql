4188f9e30525b658436eec80c8c283f2c98cb095
PRAVEEN KUMAR C A <147300351+Praveen192001@users.noreply.github.com>
2024-09-23 11:56:23 +0530

    Add files via upload

--- includes/header.php
+++ includes/header.php
@@ -1,27 +1,41 @@
-<div class="top-nav w3-agiletop">
-                <div class="container">
-                    <div class="navbar-header w3llogo">
-                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>  
-                        <h1><a href="index.php">Bus Pass Management System</a></h1> 
-                    </div>
-                    <!-- Collect the nav links, forms, and other content for toggling -->
-                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                        <div class="w3menu navbar-right">
-                            <ul class="nav navbar">
-                                <li><a href="index.php" class="active">Home</a></li>
-                                <li><a href="about.php">About</a></li> 
-                                                             
-                                <li><a href="contact.php">Contact</a></li>
-                                <li><a href="download-pass.php">View Pass</a></li>
-                                <li><a href="admin/index.php">Admin</a></li>
-                            </ul>
-                        </div> 
-                        <div class="clearfix"> </div>  
-                    </div>
-                </div>  
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation" id="navbar">
+            <!-- navbar-header -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="dashboard.php">
+                    <strong style="color: white;font-size: 30px">Bus Pass MS</strong>
+                </a>
             </div>
+            <!-- end navbar-header -->
+            <!-- navbar-top-links -->
+            <ul class="nav navbar-top-links navbar-right">
+                <!-- main dropdown -->
+                 
+   
+
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        <i class="fa fa-user fa-3x"></i>
+                    </a>
+                    <!-- dropdown user-->
+                    <ul class="dropdown-menu dropdown-user">
+                        <li><a href="admin-profile.php"><i class="fa fa-user fa-fw"></i>User Profile</a>
+                        </li>
+                        <li><a href="change-password.php"><i class="fa fa-gear fa-fw"></i>Settings</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li><a href="logout.php"><i class="fa fa-sign-out fa-fw"></i>Logout</a>
+                        </li>
+                    </ul>
+                    <!-- end dropdown-user -->
+                </li>
+                <!-- end main dropdown -->
+            </ul>
+            <!-- end navbar-top-links -->
+
+        </nav>
